647af150f1c60715f0ee6b0e9fa3cebfc808edcd
ghchae <cvxgihyun@gmail.com>
2021-08-26 20:02:18 +0900

    메소드를 활용해 구구단 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
-
 public class Gugudan {
+	public static int[] calculator(int step) {
+		int[] gugudanArray = new int[9];
+		for (int j = 0; j < gugudanArray.length; j++) {
+			gugudanArray[j] = step * (j + 1);
+		}
+		return gugudanArray;
+	}
+	public static void printGugudan(int[] gugudanArray) {
+		for (int i = 0; i < gugudanArray.length; i++) {
+			System.out.print(gugudanArray[i]);
+			if(i != (gugudanArray.length - 1)) {
+				System.out.append(", ");
+			}
+		}
+		System.out.println();
+	}
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Gugudan Start");
-		System.out.println("-------------");
-		System.out.print("출력할 구구단을 입력해주세요 : ");
-		int number = scanner.nextInt();
-		scanner.close();
-		System.out.println(number + "단을 출력합니다.");
-		for (int i = 1; i < 10; i++) {
-			System.out.println(number + " * " + i + " = " + (number * i));
+		for (int i=1; i < 10; i++) {
+			printGugudan(calculator(i));	
 		}
 	}
 }
