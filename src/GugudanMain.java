81cc62f80788d45dababe55826cde82fe1ed23fc
ghchae <cvxgihyun@gmail.com>
2021-08-27 14:05:03 +0900

    요구사항 - 사용자가 입력한 값에따라 크기가 다른 구구단을 출력

--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class GugudanMain {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("input step : ");
+		System.out.println("input step : ");
 		int step = scanner.nextInt();
+		System.out.print("input endStep : ");
+		int endStep = scanner.nextInt();
 		scanner.close();
 		
-		for (int i=1; i <= step; i++) {
-			Gugudan.printGugudan(Gugudan.calculator(i,step));	
+		for (int i = 1; i <= step; i++) {
+			Gugudan.printGugudan(Gugudan.calculator(i, endStep));	
 		}
 	}
 }
